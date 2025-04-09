083b5cbd730522bd501b1e5b0a5c4f2512b2e6a4
Hople <lequanghop844@gmail.com>
2025-04-08 21:31:28 -0500

    third upate

---
+++ target/debug/build/safemem_c-to-rust_memory_safety_demo-d44e12d5e5080583/build_script_build-d44e12d5e5080583.d
@@ -0,0 +1,5 @@
+/home/hople/Texas A&M University/working_folder/SafeMem_C-to-Rust_Memory_Safety_Demo/target/debug/build/safemem_c-to-rust_memory_safety_demo-d44e12d5e5080583/build_script_build-d44e12d5e5080583: build.rs
+
+/home/hople/Texas A&M University/working_folder/SafeMem_C-to-Rust_Memory_Safety_Demo/target/debug/build/safemem_c-to-rust_memory_safety_demo-d44e12d5e5080583/build_script_build-d44e12d5e5080583.d: build.rs
+
+build.rs:
